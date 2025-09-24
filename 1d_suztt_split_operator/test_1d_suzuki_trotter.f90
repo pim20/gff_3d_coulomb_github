8bd9e39ea3b0e8719b26b4186d406b5d91e59c2f
pim20 <priyam20@iitk.ac.in>
2025-09-24 18:20:10 +0530

    the compare subroutine for different values of m added

--- 1d_suztt_split_operator/test_1d_suzuki_trotter.f90
+++ 1d_suztt_split_operator/test_1d_suzuki_trotter.f90
@@ -11,7 +11,8 @@ implicit none
 !call test_suzuki_trotter_m1()
 !call test_suzuki_trotter_m2()
 !call test_suzuki_trotter_m3()
-call test_gfree()
+!call test_gfree()
+call compare_st_mval_w0()
 
 contains
 !_______________________________________________________________________________
@@ -20,7 +21,7 @@ contains
 subroutine test_suzuki_trotter_m1()
   implicit none
   real(8)::beta, val, x, xprime, a,b,dx
-  real(8), allocatable::xgrid(:)
+  real(8), allocatable::xgrid(:), valm1(:,:)
   integer:: nwpot, n, i,j
   nwpot=1
   beta = 1.0d0
@@ -29,17 +30,19 @@ subroutine test_suzuki_trotter_m1()
   b=2.0d0
   open(unit=1, file= "test_st_m1_w0.txt", status= "replace", action= "write")
   allocate(xgrid(n))
-  call setup_grid(xgrid, n,a,b,dx)
+  allocate(valm1(n,n))
+  !call setup_grid(xgrid, n,a,b,dx)
+  call suzuki_trotterm1(beta,nwpot, n, a,b, xgrid, valm1)
   do i =1, n
     x =xgrid(i)
     do j =1,n
       xprime = xgrid(j)
-      call suzuki_trotterm1(beta,nwpot, x, xprime, val)
-      write(1,*) x, xprime, val
+      write(1,*) x, xprime, valm1(i,j)
     end do
   end do
   !call setupgrid_call()
   deallocate(xgrid)
+  deallocate(valm1)
   close(1)
   end subroutine test_suzuki_trotter_m1
 !_______________________________________________________________________________
@@ -148,10 +151,81 @@ end subroutine test_gfree
 
 subroutine compare_st_mval_w0()
   implicit none
-  real(8)  :: beta, x, xprime
+  real(8)  :: beta
+  integer  :: n
+  real(8), allocatable:: xm(:), xp(:)
+  real(8), allocatable:: val_m1(:,:), val_m2(:,:), val_m3(:,:), val_gf(:,:)
+  real(8):: diff1, diff2, diff3,sum1, sum2, sum3
   real(8)  :: gf0
   real(8)  :: a,b
-  real(8)  ::
+  integer:: i,j
+  ! Same grid size as in test runs
+  n = 100
+
+  allocate(xm(n), xp(n))
+  allocate(val_m1(n,n), val_m2(n,n), val_m3(n,n), val_gf(n,n))
+  !real(8)  ::
+  ! read the file m=1
+  open(unit=10, file="test_st_m1_w0.txt", status="old", action="read")
+  do i = 1, n
+     do j = 1, n
+        read(10,*) xm(i), xp(j), val_m1(i,j)
+     end do
+  end do
+  close(10)
+
+  !read m=2
+  open(unit=11, file= "test_st_m2_w0.txt", status ="old", action= "read")
+  do i= 1, n
+    do j=1, n
+      read(11,*) xm(i), xp(j), val_m2(i,j)
+    end do
+  end do
+  close(11)
+
+  ! Read m=3
+    open(unit=12, file="test_st_m3_w0.txt", status="old", action="read")
+    do i = 1, n
+       do j = 1, n
+          read(12,*) xm(i), xp(j), val_m3(i,j)
+       end do
+    end do
+    close(12)
+
+    ! Read free kernel
+    open(unit=13, file="test_gfree_func.txt", status="old", action="read")
+    do i = 1, n
+       do j = 1, n
+          read(13,*) xm(i), xp(j), val_gf(i,j)
+       end do
+    end do
+    close(13)
+    !now for the diff
+
+    sum1 =0.0d0
+    sum2 =0.0d0
+    sum3 =0.0d0
+    open(unit =15, file= "compare_diff_mval_with_gfree.txt", status="replace", action="write")
+    do i = 1, n
+     do j = 1, n
+        diff1 = abs(val_m1(i,j) - val_gf(i,j))
+        diff2 = abs(val_m2(i,j) - val_gf(i,j))
+        diff3 = abs(val_m3(i,j) - val_gf(i,j))
+
+        sum1 = sum1+diff1
+        sum2 = sum2+diff2
+        sum3 = sum3+diff3
+        write(15,*) xm(i), xp(j), diff1, diff2, diff3
+     end do
+   end do
+
+    close(15)
+
+    print *, "Sum(|m1 - gfree|) = ", sum1
+    print *, "Sum(|m2 - gfree|) = ", sum2
+    print *, "Sum(|m3 - gfree|) = ", sum3
+    deallocate(xm, xp, val_m1, val_m2, val_m3, val_gf)
+
 
 end subroutine
 
